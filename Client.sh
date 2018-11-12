0f5fa62ff00b114062c787dcfa892e486e138952
Necrolord <braun.eran@gmail.com>
2018-11-12 18:02:14 +0200

    initial commit

---
+++ Client.sh
@@ -0,0 +1,29 @@
+################################################################################
+# Author: Necrolord
+# Date: 11/11/2018
+# Version: 0.0.1
+# Goal: Installing zabbix client.
+# Comments: Please refer the Additional Notes file.
+################################################################################
+
+#!/bin/bash
+
+sed -i -e 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+
+read -p "Enter hostname : " hostname
+echo "$hostname.localdomain" > /etc/hostname
+setenforce 0
+firewall-cmd --add-port=10050/tcp --permanent
+rpm -ivh https://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm
+
+dnf install zabbix-agent -y
+
+sed -i -e 's/# EnableRemoteCommands=0/EnableRemoteCommands=0/g' /etc/zabbix/zabbix_agentd.conf
+sed -i -e 's/ServerActive=127.0.0.1/ServerActive=10.0.0.5/g' /etc/zabbix/zabbix_agentd.conf
+sed -i -e "s/Hostname=Zabbix server/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
+
+systemctl enable zabbix-agent
+systemctl start zabbix-agent
+
+
+echo "Done"
