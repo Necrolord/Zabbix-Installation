a2c0dd5606dee31d4cb11ec0cfc92450c94f1971
Necrolord <braun.eran@gmail.com>
2018-11-12 18:22:59 +0200

    initial commit

---
+++ Server.sh
@@ -0,0 +1,60 @@
+################################################################################
+# Author: Necrolord
+# Date: 12/11/2018
+# Version: 0.0.1
+# Goal: Installing zabbix server.
+# Comments:
+################################################################################
+#!/bin/bash
+
+sed -i -e 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+setenforce 0
+firewall-cmd --add-port=10051/tcp --permanent
+firewall-cmd --add-port=80/tcp --permanent
+rpm -ivh https://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm
+
+yum install yum-utils -y
+
+yum-config-manager --enable rhel-7-server-optional-rpms
+
+yum install zabbix-server-mysql mariadb mariadb-server -y
+
+systemctl enable mariadb --now
+
+mysql_secure_installation <<_EOF_
+y
+password
+password
+y
+y
+y
+y
+_EOF_
+
+
+echo ""
+echo "Enter root SQL password"
+echo ""
+mysql -u root -ppassword -e "create database zabbix character set utf8 collate utf8_bin;"
+mysql -u root -ppassword -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';"
+
+echo ""
+echo "Enter zabbix SQL password"
+echo ""
+zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -pzabbix zabbix
+
+sed -i -e 's/# DBHost=localhost/DBHost=localhost/g' /etc/zabbix/zabbix_server.conf
+
+sed -i -e 's/# DBPassword=/DBPassword=zabbix/g' /etc/zabbix/zabbix_server.conf
+
+systemctl enable zabbix-server --now
+
+yum install httpd -y
+systemctl enable httpd --now
+yum install zabbix-web-mysql -y
+sed -i -e 's/# php_value date.timezone Europe\/Riga/php_value date.timezone Asia\/Jerusalem/g' /etc/httpd/conf.d/zabbix.conf
+systemctl restart httpd
+
+setsebool -P httpd_can_connect_zabbix on
+firewall-cmd --permanent --add-port=10050/tcp
+firewall-cmd --permanent --add-port=10051/tcp
